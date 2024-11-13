51210e4f981e17df214b8aa618094f06d20900e8
Sergii Illichevskyi <sergii.illichevskyi@graviteesource.com>
2024-11-13 23:46:05 +0100

    feat: create transform status code policy

---
+++ src/test/java/io/gravitee/policy/status/code/StatusCodePolicyIntegrationTest.java
@@ -0,0 +1,114 @@
+package io.gravitee.policy.status.code;
+
+import io.gravitee.apim.gateway.tests.sdk.AbstractGatewayTest;
+import io.gravitee.apim.gateway.tests.sdk.configuration.GatewayConfigurationBuilder;
+import io.gravitee.definition.model.Api;
+import io.gravitee.definition.model.flow.Flow;
+import io.gravitee.definition.model.flow.Operator;
+import io.gravitee.definition.model.flow.PathOperator;
+import io.gravitee.definition.model.flow.Step;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.rxjava3.core.Vertx;
+import io.vertx.rxjava3.core.http.HttpClient;
+import io.vertx.rxjava3.core.http.HttpClientRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Set;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static io.vertx.core.http.HttpMethod.GET;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StatusCodePolicyIntegrationTest extends AbstractGatewayTest {
+
+    @Override
+    @BeforeEach
+    public void setUp(Vertx vertx) throws Exception {
+        this.applicationContext = new AnnotationConfigApplicationContext();
+        super.setUp(vertx);
+    }
+
+    @Override
+    protected void configureGateway(GatewayConfigurationBuilder gatewayConfigurationBuilder) {
+        gatewayConfigurationBuilder.set("http.port", gatewayPort());
+    }
+
+    @BeforeEach
+    public void init() {
+        // Set up WireMock to return a 202 Accepted response for the /test endpoint
+        wiremock.stubFor(get(urlEqualTo("/test")).willReturn(aResponse().withStatus(202).withBody("Accepted")));
+    }
+
+    @Test
+    void shouldTransformStatusCode() {
+        // Create an HttpClient using Vertx
+        HttpClientOptions options = new HttpClientOptions();
+        this.configureHttpClient(options);
+        HttpClient client = vertx.createHttpClient(options);
+
+        // Send a request to the gateway
+        var response = client
+                .request(GET, gatewayPort(), "localhost", "/test")
+                .flatMap(HttpClientRequest::send)
+                .flatMap(resp -> resp.body().map(body -> java.util.Map.entry(resp, body)))
+                .blockingGet();
+
+        // Then
+        assertThat(response.getKey().statusCode()).isEqualTo(200); // Verify the status code is transformed to 200
+        String body = response.getValue().toString();
+        assertThat(body).isEqualTo("Accepted"); // Verify the response body is unchanged
+    }
+
+    @Override
+    public void configureApi(Api api) {
+        // Set the API's backend to point to the mock backend
+        api
+                .getProxy()
+                .getGroups()
+                .iterator()
+                .next()
+                .getEndpoints()
+                .iterator()
+                .next()
+                .setTarget("http://localhost:" + wiremock.port());
+
+        // Configure the API with a flow that applies the StatusCodePolicy on the RESPONSE phase
+        Flow flow = new Flow();
+        flow.setName("Transform status code");
+        flow.setEnabled(true);
+
+        // Set the path operator
+        PathOperator pathOperator = new PathOperator();
+        pathOperator.setPath("/");
+        pathOperator.setOperator(Operator.STARTS_WITH);
+        flow.setPathOperator(pathOperator);
+
+        // Set the methods
+        flow.setMethods(Set.of(io.gravitee.common.http.HttpMethod.GET));
+
+        // Create the step for the policy
+        Step step = new Step();
+        step.setName("status-code");
+        step.setPolicy("status-code-policy");
+        // Configure the policy with mappings to transform 202 to 200
+        step.setConfiguration(
+                "{\n" +
+                        "  \"statusMappings\": [\n" +
+                        "    {\n" +
+                        "      \"inputStatusCode\": 202,\n" +
+                        "      \"outputStatusCode\": 200\n" +
+                        "    }\n" +
+                        "  ]\n" +
+                        "}"
+        );
+        step.setEnabled(true);
+
+        // Since the policy needs to run on the RESPONSE phase, we add it to the 'post' steps
+        flow.setPost(List.of(step));
+
+        api.setFlows(List.of(flow));
+    }
+}
